a68e9534f7926976d0c5cd63dbe3c595d1b06091
[Hayan] <[c_white723@naver.com]>
2023-06-05 20:28:44 +0900

    feat: CORS 설정

---
+++ src/main/java/hayan/todoApp/config/WebConfig.java
@@ -0,0 +1,14 @@
+package hayan.todoApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("https://todobackend.com/");
+    }
+}
